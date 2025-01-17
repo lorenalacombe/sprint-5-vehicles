0fc246d2e98c6222a37b2d363ce57c7a4ad3704d
lorenalacombe <139777577+lorenalacombe@users.noreply.github.com>
2025-01-17 11:38:16 -0300

    Update app.py
    Cabeçalho adicionado

--- app.py
+++ app.py
@@ -2,6 +2,7 @@ import pandas as pd
 import plotly.express as px
 import streamlit as st
 
+st.header("Análise de Dados de Veículos Usados") #cabeçalho adicionado
 car_data = pd.read_csv('vehicles_us.csv') # lendo os dados
 hist_button = st.button('Criar histograma') # criar um botão
 
